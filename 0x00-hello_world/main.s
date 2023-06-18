9b3d1ed6b141236a4994fa63e64db511e6eaa11e
none <somebody4xxxx@gmail.com>
2023-06-18 11:00:45 -0700

    2. Assembler

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,30 +6,30 @@
  */
 int main(void)
 {
-    return (0);
+return (0);
 }
 julien@ubuntu:~/c/0x00$ ./2-assembler
 julien@ubuntu:~/c/0x00$ ls
 0-preprocessor  1-compiler  2-assembler c  main.c  main.s  Makefile
 julien@ubuntu:~/c/0x00$ cat main.s
-    .file   "main.c"
-    .text
-    .globl  main
-    .type   main, @function
+.file   "main.c"
+.text
+.globl  main
+.type   main, @function
 main:
 .LFB0:
-    .cfi_startproc
-    pushq   %rbp
-    .cfi_def_cfa_offset 16
-    .cfi_offset 6, -16
-    movq    %rsp, %rbp
-    .cfi_def_cfa_register 6
-    movl    $0, %eax
-    popq    %rbp
-    .cfi_def_cfa 7, 8
-    ret
-    .cfi_endproc
+.cfi_startproc
+pushq   %rbp
+.cfi_def_cfa_offset 16
+.cfi_offset 6, -16
+movq    %rsp, %rbp
+.cfi_def_cfa_register 6
+movl    $0, %eax
+popq    %rbp
+.cfi_def_cfa 7, 8
+ret
+.cfi_endproc
 .LFE0:
-    .size   main, .-main
-    .ident  "GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609"
-    .section    .note.GNU-stack,"",@progbits
+.size   main, .-main
+.ident  "GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609"
+.section    .note.GNU-stack,"",@progbits
