1378908e5e7243ad042727f35047f2f4368109f4
none <somebody4xxxx@gmail.com>
2023-06-18 15:44:11 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-char a ;
-char b ;
-for(a = 'a'; a == 'z' ;a++)
+char a;
+char b;
+for(a = 'a'; a == 'z'; a++)
 {
 putchar(a);
 }
-for(b = 'A'; b == 'Z';b++)
+for(b = 'A'; b == 'Z'; b++)
 {
 putchar(b);
 }
