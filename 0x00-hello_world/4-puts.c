132e57836550d8eae6194b445bbd9fa1860f44a6
none <somebody4xxxx@gmail.com>
2023-06-18 09:42:27 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -8,7 +7,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
 
