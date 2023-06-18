4bf965a6676412a151594e6a029c3187bc6c878c
none <somebody4xxxx@gmail.com>
2023-06-18 14:56:04 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a ;
+char b ;
+for(a='a',a='z',a++)
+{
+putchar(a);
+}
+for(b='A',b='Z',b++)
+{
+putchar(b);
+}
+putchar('\n');
+return (0);
+}
+
