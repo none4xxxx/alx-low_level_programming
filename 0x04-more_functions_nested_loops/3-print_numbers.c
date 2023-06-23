66885a0a26972b3ff88f6f0cdfdf25245d6ee476
none <somebody4xxxx@gmail.com>
2023-06-23 09:18:44 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+int i=0;
+for (i == 0; i <= 9; i++)
+{
+_putchar ("%d", i);
+}}
+
