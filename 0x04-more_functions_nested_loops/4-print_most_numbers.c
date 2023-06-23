4601c48149b3198f4e3b3def7d913a9736162db0
none <somebody4xxxx@gmail.com>
2023-06-23 09:58:47 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
 */
 void print_most_numbers(void)
 {
-char i;
-for (i == '0'; i < '10'; i++)
+int i;
+for (i == 0; i < 10; i++)
 {
-if (i != '2' && i != '4')
+if (i != 2 && i != 4)
 {
-_putchar(i);
+_putchar("%d", i);
 }
 }
 _putchar('\n');
