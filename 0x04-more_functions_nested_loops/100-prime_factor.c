8f13bdd5d06bd1e75cd2aeefd8600a46be75c81e
none <somebody4xxxx@gmail.com>
2023-06-23 14:36:37 -0700

    11. The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,47 +1,27 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdint.h>
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- */
-void print_times_table(int n)
+ * main - Write a program that finds and prints the largest prime factor
+ *
+ * number 612852475143, followed by a new line.
+ *
+ * Return: 0
+ *
+ **/
+int main(void)
 {
-	int i, j, k;
+	int newValue, count;
+	long int number = 612852475143;
 
-	if (n >= 0 && n <= 15)
+	for (count = 2; count <= number; count++)
 	{
-		for (i = 0; i <= n; i++)
+		if (number % count == 0)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
+			number = number / count;
+			newValue = count;
 		}
 	}
+	printf("%d\n", newValue);
+	return (0);
 }
